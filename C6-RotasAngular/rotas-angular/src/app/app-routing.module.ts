166a61556b8006e64d8850aad98b0f5968942427
André Bueno <andrebueno99@gmail.com>
2023-05-19 16:27:08 -0300

    Upload 19/05

---
+++ C6-RotasAngular/rotas-angular/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portfolio/card/card.component';
+import { MenuComponent } from './shared/menu/menu.component'
+const routes: Routes = [
+  {
+    path: "",
+    component: TitleComponent,
+    pathMatch: "full"
+
+  },
+  {
+
+    path: "portfolio",
+    component: CardComponent,
+    children:[{path: ":id",component: CardComponent,}, 
+    {path: ":id/:token",component: CardComponent,}]
+    
+
+
+  },
+  {
+
+    path: "**",
+    redirectTo: ""
+
+
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
